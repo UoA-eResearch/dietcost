52706318862ece1696cccd4946c7d1c799093b42
Nick Young <nick.young@auckland.ac.nz>
2017-08-10 16:35:33 +1200

    remove echo statements

--- run_all.sh
+++ run_all.sh
@@ -6,10 +6,10 @@ declare -a personas=("adult man" "adult women" "14 boy" "7 girl")
 
 for i in {1..5};do
   for p in "${personas[@]}";do
-    echo ./meal_planner.py --iterations $iterations --folder 1M --dataset dataset_general_0healthy.xlsx --persona "$p" --discretionary 0  --no-takeaways --alcohol 0
+    ./meal_planner.py --iterations $iterations --folder 1M --dataset dataset_general_0healthy.xlsx --persona "$p" --discretionary 0  --no-takeaways --alcohol 0
   done
   for p in "${personas[@]}";do
     p="$p C"
-    echo ./meal_planner.py --iterations $iterations --folder 1M --dataset dataset_general_0healthy.xlsx --persona "$p" --discretionary 100  --takeaways --alcohol 0
+    ./meal_planner.py --iterations $iterations --folder 1M --dataset dataset_general_0healthy.xlsx --persona "$p" --discretionary 100  --takeaways --alcohol 0
   done
 done
