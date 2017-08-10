20e0d862a7dbbc4bcbcfac8e85fe6ec1eb09ce14
Nick Young <nick.young@auckland.ac.nz>
2017-08-10 16:43:07 +1200

    configure logging

--- run_all.sh
+++ run_all.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 iterations=1000000
+name=1M
 
 declare -a personas=("adult man" "adult women" "14 boy" "7 girl")
 
+mkdir -p $name/logs
+
 for i in {1..5};do
   for p in "${personas[@]}";do
-    ./meal_planner.py --iterations $iterations --folder 1M --dataset dataset_general_0healthy.xlsx --persona "$p" --discretionary 0  --no-takeaways --alcohol 0
+    ./meal_planner.py --iterations $iterations --folder $name --dataset dataset_general_0healthy.xlsx --persona "$p" --discretionary 0  --no-takeaways --alcohol 0 > "$name/logs/${p}_${i}.log" &
   done
   for p in "${personas[@]}";do
     p="$p C"
-    ./meal_planner.py --iterations $iterations --folder 1M --dataset dataset_general_0healthy.xlsx --persona "$p" --discretionary 100  --takeaways --alcohol 0
+    ./meal_planner.py --iterations $iterations --folder $name --dataset dataset_general_0healthy.xlsx --persona "$p" --discretionary 100  --takeaways --alcohol 0  > "$name/logs/${p}_${i}.log" &
   done
 done
