c91108ae459b9f32f21a43dd23f44f6b78693cc1
Nick Young <nick.young@auckland.ac.nz>
2017-08-23 13:35:14 +1200

    update datasets for fixed food group protein constraint

--- run_all.sh
+++ run_all.sh
@@ -2,6 +2,9 @@
 
 iterations=1000000
 name=1M
+dataset=dataset_general.xlsx
+#dataset=dataset_Maori.xlsx
+#dataset=dataset_Pacific.xlsx
 
 declare -a personas=("adult man" "adult women" "14 boy" "7 girl")
 
@@ -9,10 +12,10 @@ mkdir -p $name/logs
 
 for i in {1..5};do
   for p in "${personas[@]}";do
-    ./meal_planner.py --iterations $iterations --folder $name --dataset dataset_general_0healthy.xlsx --persona "$p" --discretionary 0  --no-takeaways --alcohol 0 > "$name/logs/${p}_${i}.log" &
+    ./meal_planner.py --iterations $iterations --folder $name --dataset $dataset --persona "$p" --discretionary 0  --no-takeaways --alcohol 0 > "$name/logs/${p}_${i}.log" &
   done
   for p in "${personas[@]}";do
     p="$p C"
-    ./meal_planner.py --iterations $iterations --folder $name --dataset dataset_general_0healthy.xlsx --persona "$p" --discretionary 100  --takeaways --alcohol 0  > "$name/logs/${p}_${i}.log" &
+    ./meal_planner.py --iterations $iterations --folder $name --dataset $dataset --persona "$p" --discretionary 100  --takeaways --alcohol 0  > "$name/logs/${p}_${i}.log" &
   done
 done
