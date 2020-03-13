4dce0a674f0589b55d7541db44f42feced9c35f0
Nick Young <nick.young@auckland.ac.nz>
2020-03-13 13:04:46 +1300

    update run_all script for mass runs

--- run_all.sh
+++ run_all.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-iterations=1000000
-name=1M
-dataset=dataset_general.xlsx
-#dataset=dataset_Maori.xlsx
-#dataset=dataset_Pacific.xlsx
+iterations="1e6"
 
 declare -a personas=("adult man" "adult women" "14 boy" "7 girl")
 
-mkdir -p $name/logs
-
-for i in {1..5};do
+for i in {1..40};do
   for p in "${personas[@]}";do
-    ./meal_planner.py --iterations $iterations --folder $name --dataset $dataset --persona "$p" --discretionary 0  --no-takeaways --alcohol 0 > "$name/logs/${p}_${i}.log" &
-  done
-  for p in "${personas[@]}";do
-    p="$p C"
-    ./meal_planner.py --iterations $iterations --folder $name --dataset $dataset --persona "$p" --discretionary 100  --takeaways --alcohol 0  > "$name/logs/${p}_${i}.log" &
+    pv="$p PV"
+    pf="$p PF"
+    mkdir -p "$pv" "$pf"
+    echo "python3.6 meal_planner.py --iterations $iterations --folder \"$pf\" --persona \"$pf\" > \"$pf/$i.log\""
+    echo "python3.6 meal_planner.py --iterations $iterations --folder \"$pv\" --persona \"$pv\" > \"$pv/$i.log\""
   done
 done
