e9feb41f8159782e7899c7d9030c29ffd559f813
Nick Young <nick.young@auckland.ac.nz>
2020-03-16 10:15:16 +1300

    runs folder

--- run_all.sh
+++ run_all.sh
@@ -6,10 +6,9 @@ declare -a personas=("adult man" "adult women" "14 boy" "7 girl")
 
 for i in {1..40};do
   for p in "${personas[@]}";do
-    pv="$p PV"
-    pf="$p PF"
-    mkdir -p "$pv" "$pf"
-    echo "python3.6 meal_planner.py --iterations $iterations --folder \"$pf\" --persona \"$pf\" > \"$pf/$i.log\""
-    echo "python3.6 meal_planner.py --iterations $iterations --folder \"$pv\" --persona \"$pv\" > \"$pv/$i.log\""
+    pc="$p C"
+    mkdir -p "runs/$p" "runs/$pc"
+    echo "python3.6 meal_planner.py --iterations $iterations --folder \"runs/$p\" --persona \"$p\" --alcohol 0 > \"runs/$p/$i.log\""
+    echo "python3.6 meal_planner.py --iterations $iterations --folder \"runs/$pc\" --persona \"$pc\" --alcohol 0 > \"runs/$pc/$i.log\""
   done
 done
